6d97468f81ef0cd61c554c2e02e2826be5dae3b8
amihaylovaa <aggmihaylova@gmail.com>
2023-01-21 22:19:48 +0200

    more indentation fixes

--- app.py
+++ app.py
@@ -23,107 +23,107 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 @app.route("/elevation-service/linear-route/",  methods=['POST'])
 def get_elevation_linear_route():        
-        ET.register_namespace('', GPX_NAMESPACE)        
+    ET.register_namespace('', GPX_NAMESPACE)        
         
-        gpx_file = request.files.get('gpx_file')
+    gpx_file = request.files.get('gpx_file')
         
-        if gpx_file == None:
-                return send_error_response('Gpx file not set', StatusCode.BAD_REQUEST)
+    if gpx_file == None:
+        return send_error_response('Gpx file not set', StatusCode.BAD_REQUEST)
 
-        tree = None
-        try:
-                tree = ET.parse(gpx_file)
-        except:
-               return send_error_response('Invalid gpx', StatusCode.BAD_REQUEST)
+    tree = None
+    try:
+        tree = ET.parse(gpx_file)
+    except:
+        return send_error_response('Invalid gpx', StatusCode.BAD_REQUEST)
 
-        root = tree.getroot()
-        track_points = extract_track_points(root)
-        elevations = extract_elevation(root)
+    root = tree.getroot()
+    track_points = extract_track_points(root)
+    elevations = extract_elevation(root)
 
-        if len(track_points) == 0:
-              return send_error_response('Track points are not added', StatusCode.BAD_REQUEST)
+    if len(track_points) == 0:
+        return send_error_response('Track points are not added', StatusCode.BAD_REQUEST)
 
-        elevations_jaxa =  extract_elevations_from_dem(DemFileName.ALOS_WORLD, track_points)
-        elevations_srtm_90_m = extract_elevations_from_dem(DemFileName.SRTM_90_M, track_points)
-        elevations_srtm_30_m = extract_elevations_from_dem(DemFileName.SRTM_30_M, track_points)
-        elevations_map = { DEMDataSource.SRTM_30_M: elevations_srtm_30_m, DEMDataSource.SRTM_90_M: elevations_srtm_90_m, DEMDataSource.ALOS_WORLD_3D_30_M: elevations_jaxa }
-        approximated_elevations = get_approximated_elevations(elevations_map, track_points)
+    elevations_jaxa =  extract_elevations_from_dem(DemFileName.ALOS_WORLD, track_points)
+    elevations_srtm_90_m = extract_elevations_from_dem(DemFileName.SRTM_90_M, track_points)
+    elevations_srtm_30_m = extract_elevations_from_dem(DemFileName.SRTM_30_M, track_points)
+    elevations_map = { DEMDataSource.SRTM_30_M: elevations_srtm_30_m, DEMDataSource.SRTM_90_M: elevations_srtm_90_m, DEMDataSource.ALOS_WORLD_3D_30_M: elevations_jaxa }
+    approximated_elevations = get_approximated_elevations(elevations_map, track_points)
                 
-        if len(elevations) == 0:
-                add_elevation_element(root, approximated_elevations)
-        else:
-                replace_existing_elevations(root, approximated_elevations)
+    if len(elevations) == 0:
+        add_elevation_element(root, approximated_elevations)
+    else:
+        replace_existing_elevations(root, approximated_elevations)
 
-        gpx_file.seek(0)
-        gpx_file.truncate(0)
-        tree.write(gpx_file)
-        gpx_file.seek(0)
-        file_content = gpx_file.read()
+    gpx_file.seek(0)
+    gpx_file.truncate(0)
+    tree.write(gpx_file)
+    gpx_file.seek(0)
+    file_content = gpx_file.read()
 
-        return send_file(BytesIO(file_content), mimetype=MimeType.GPX_XML, as_attachment=True, download_name=gpx_file.filename)
+    return send_file(BytesIO(file_content), mimetype=MimeType.GPX_XML, as_attachment=True, download_name=gpx_file.filename)
 
 @app.route("/elevation-service/closed-contour-route/",  methods=['POST'])
 def get_elevation_closed_contour_route():
-        ET.register_namespace('', GPX_NAMESPACE)        
+    ET.register_namespace('', GPX_NAMESPACE)        
 
-        gpx_file = request.files.get('gpx_file')
-        extracted_offset = request.form.get('offset')
+    gpx_file = request.files.get('gpx_file')
+    extracted_offset = request.form.get('offset')
 
-        if gpx_file == None and extracted_offset == None:
-                return send_error_response('Gpx file and offset are not set', StatusCode.BAD_REQUEST)
+    if gpx_file == None and extracted_offset == None:
+        return send_error_response('Gpx file and offset are not set', StatusCode.BAD_REQUEST)
 
-        if gpx_file == None:
-                return send_error_response('Gpx file not set', StatusCode.BAD_REQUEST)
+    if gpx_file == None:
+        return send_error_response('Gpx file not set', StatusCode.BAD_REQUEST)
         
-        if extracted_offset == None:
-                return send_error_response('Offset not set', StatusCode.BAD_REQUEST)
+    if extracted_offset == None:
+        return send_error_response('Offset not set', StatusCode.BAD_REQUEST)
 
-        if not extracted_offset.isdigit():
-                return send_error_response('Offset cannot contain letters or symbols', StatusCode.BAD_REQUEST)
+    if not extracted_offset.isdigit():
+        return send_error_response('Offset cannot contain letters or symbols', StatusCode.BAD_REQUEST)
 
-        offset = int(extracted_offset)
+    offset = int(extracted_offset)
 
-        if offset != MIN_OFFSET and offset != MAX_OFFSET:
-                offset = MIN_OFFSET
+    if offset != MIN_OFFSET and offset != MAX_OFFSET:
+        offset = MIN_OFFSET
 
-        tree = None
-        try:
-                tree = ET.parse(gpx_file)
-        except:
-               return send_error_response('Invalid gpx', StatusCode.BAD_REQUEST)
+    tree = None
+    try:
+        tree = ET.parse(gpx_file)
+    except:
+        return send_error_response('Invalid gpx', StatusCode.BAD_REQUEST)
 
-        root = tree.getroot()
-        track_points = extract_track_points(root)
+    root = tree.getroot()
+    track_points = extract_track_points(root)
         
-        if len(track_points) < MIN_POINTS_COUNT:
-              return send_error_response('At least 3 points are required', StatusCode.BAD_REQUEST)
+    if len(track_points) < MIN_POINTS_COUNT:
+        return send_error_response('At least 3 points are required', StatusCode.BAD_REQUEST)
 
-        bounding_box = get_bounding_box(track_points)
-        lattice_size = calculate_lattice_size(bounding_box)
-        lattice = generate_square_lattice(int(offset), int(lattice_size), bounding_box)
-        lattice_as_list = convert_to_list(lattice)
-        cleared_points = clear_points(track_points, lattice_as_list)
-        restored_lattice = restore_square_lattice(int(offset), int(lattice_size), cleared_points, lattice)
-        valid_lattice = validate_lattice(float(int(offset) + 0.5), restored_lattice)
+    bounding_box = get_bounding_box(track_points)
+    lattice_size = calculate_lattice_size(bounding_box)
+    lattice = generate_square_lattice(int(offset), int(lattice_size), bounding_box)
+    lattice_as_list = convert_to_list(lattice)
+    cleared_points = clear_points(track_points, lattice_as_list)
+    restored_lattice = restore_square_lattice(int(offset), int(lattice_size), cleared_points, lattice)
+    valid_lattice = validate_lattice(float(int(offset) + 0.5), restored_lattice)
 
-        if valid_lattice == None or len(valid_lattice) == 0:
-                return send_error_response("Cannot generate lattice, please try again with another route or offset", StatusCode.UNPROCESSABLE_ENTITY)
+    if valid_lattice == None or len(valid_lattice) == 0:
+        return send_error_response("Cannot generate lattice, please try again with another route or offset", StatusCode.UNPROCESSABLE_ENTITY)
 
-        elevations_jaxa =  extract_elevations_from_dem(DemFileName.ALOS_WORLD, track_points)
-        elevations_srtm_90_m = extract_elevations_from_dem(DemFileName.SRTM_90_M, track_points)
-        elevations_srtm_30_m = extract_elevations_from_dem(DemFileName.SRTM_30_M, track_points)
-        elevations_map = { DEMDataSource.SRTM_30_M: elevations_srtm_30_m, DEMDataSource.SRTM_90_M: elevations_srtm_90_m, DEMDataSource.ALOS_WORLD_3D_30_M: elevations_jaxa }
-        approximated_elevations = get_approximated_elevations(elevations_map, valid_lattice)
+    elevations_jaxa =  extract_elevations_from_dem(DemFileName.ALOS_WORLD, track_points)
+    elevations_srtm_90_m = extract_elevations_from_dem(DemFileName.SRTM_90_M, track_points)
+    elevations_srtm_30_m = extract_elevations_from_dem(DemFileName.SRTM_30_M, track_points)
+    elevations_map = { DEMDataSource.SRTM_30_M: elevations_srtm_30_m, DEMDataSource.SRTM_90_M: elevations_srtm_90_m, DEMDataSource.ALOS_WORLD_3D_30_M: elevations_jaxa }
+    approximated_elevations = get_approximated_elevations(elevations_map, valid_lattice)
         
-        add_track_points(root, approximated_elevations, valid_lattice)
+    add_track_points(root, approximated_elevations, valid_lattice)
 
-        gpx_file.seek(0)
-        gpx_file.truncate(0)
-        tree.write(gpx_file)
-        gpx_file.seek(0)
-        file_content = gpx_file.read()
+    gpx_file.seek(0)
+    gpx_file.truncate(0)
+    tree.write(gpx_file)
+    gpx_file.seek(0)
+    file_content = gpx_file.read()
 
-        return send_file(BytesIO(file_content), mimetype=MimeType.GPX_XML, as_attachment=True, download_name=gpx_file.filename)
+    return send_file(BytesIO(file_content), mimetype=MimeType.GPX_XML, as_attachment=True, download_name=gpx_file.filename)
 
 def send_error_response(message, status_code):
     body = {"message": message, "status_code": status_code}
